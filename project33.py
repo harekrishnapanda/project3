a5e6b717e0977c5f088f290e9938966902c24585
Harekrishna Panda <38816064+harekrishnapanda@users.noreply.github.com>
2018-09-20 02:05:30 +0530

    Update project33.py

--- project33.py
+++ project33.py
@@ -1,142 +1,155 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep  9 16:27:13 2018
+Created on Wed Sep 9 10:22:05 2018
 
 @author: Harekrishna
 """
-
-# Import
-import tensorflow as tf
+# Importing the liabraries
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
+import pandas as pd 
 
-# Import data
-data = pd.read_csv('data_stocks.csv')
+# Reading the data
+df = pd.read_csv("adult.data",na_values="?", skipinitialspace=True)
+df.info()   # df.isnull().sum()
 
-# Drop date variable
-data = data.drop(['DATE'], 1)
+# Renaming the columns correctly
+df.columns = ['Age', 'Workclass', 'fnlwgt', 'Education', 'Education-num', 'Marital-status', \
+              'Occupation', 'Relationship', 'Race', 'Sex', 'Capital-gain', 'Capital-loss', \
+              'Hours-per-week', 'Native-country', 'Earning']
 
-# Dimensions of dataset
-n = data.shape[0]
-p = data.shape[1]
+rows_before_droppinig = df.shape[0]
+# Printing all the columns
+print(df.columns)
 
-# Make data a np.array
-data = data.values
+# Checking the NA values
+df.isnull().sum()
+df1= df.corr()
 
-# Training and test data
-train_start = 0
-train_end = int(np.floor(0.8*n))
-test_start = train_end + 1
-test_end = n
-data_train = data[np.arange(train_start, train_end), :]
-data_test = data[np.arange(test_start, test_end), :]
+# Dropping the NA values which is actually 7.368% of total Data
+# It is observed that in most of the missing data set, 
+# the ‘workclass’ variable and ‘occupation’ variable are missing data together. 
+# And the remaining have ‘nativecountry’ variable missing. We could handle the 
+# missing values by imputing the data. However, since ‘workclass’, ‘occupation’ 
+# and ‘nativecountry’ could potentially be very good predictors of income, 
+# imputing may simply skew the model.
 
-# Scale data
-scaler = MinMaxScaler(feature_range=(-1, 1))
-scaler.fit(data_train)
-data_train = scaler.transform(data_train)
-data_test = scaler.transform(data_test)
+df_opt = df.dropna()
+rows_after_droppinig = df_opt.shape[0]
 
-# Build X and y
-X_train = data_train[:, 1:]
-y_train = data_train[:, 0]
-X_test = data_test[:, 1:]
-y_test = data_test[:, 0]
+# Checking the NA values
+df_opt.isnull().sum()
 
-# Number of stocks in training data
-n_stocks = X_train.shape[1]
+# number of rows dropped
+diff = rows_before_droppinig - rows_after_droppinig
+print("Total number of rows dropped is {} wich is {:.4}% of total rows."   \
+              .format(diff,(diff/rows_before_droppinig)*100))
+print('-'*50)
 
-# Neurons
-n_neurons_1 = 1024
-n_neurons_2 = 512
-n_neurons_3 = 256
-n_neurons_4 = 128
+# applying one hot and lebel encoding on these columns.
+# Fist print the unique values of each features.
+cat_features = ['Workclass', 'Education', 'Marital-status', 'Occupation', 'Relationship',\
+                'Race', 'Sex', 'Native-country', 'Earning' ]
+print("Total numbers of unique values in catagorical features are: ")
 
-# Session
-net = tf.InteractiveSession()
+for feature in cat_features:
+    print(feature + "has total : " + str(len(df[feature].unique())))
 
-# Placeholder
-X = tf.placeholder(dtype=tf.float32, shape=[None, n_stocks])
-Y = tf.placeholder(dtype=tf.float32, shape=[None])
+# Applying One hot encoding on only catagorical columns, i.e. preferred_foot
+from sklearn.preprocessing import LabelEncoder
+label_encoder = LabelEncoder()
 
-# Initializers
-sigma = 1
-weight_initializer = tf.variance_scaling_initializer(mode="fan_avg", distribution="uniform", scale=sigma)
-bias_initializer = tf.zeros_initializer()
+for feature in cat_features:
+    #feature = 'Workclass'
+    df_opt[feature] = label_encoder.fit_transform(df_opt[feature])
+    
 
-# Hidden weights
-W_hidden_1 = tf.Variable(weight_initializer([n_stocks, n_neurons_1]))
-bias_hidden_1 = tf.Variable(bias_initializer([n_neurons_1]))
-W_hidden_2 = tf.Variable(weight_initializer([n_neurons_1, n_neurons_2]))
-bias_hidden_2 = tf.Variable(bias_initializer([n_neurons_2]))
-W_hidden_3 = tf.Variable(weight_initializer([n_neurons_2, n_neurons_3]))
-bias_hidden_3 = tf.Variable(bias_initializer([n_neurons_3]))
-W_hidden_4 = tf.Variable(weight_initializer([n_neurons_3, n_neurons_4]))
-bias_hidden_4 = tf.Variable(bias_initializer([n_neurons_4]))
+# Data after one hot encodng of all catagorical data
+df_opt.head()
 
-# Output weights
-W_out = tf.Variable(weight_initializer([n_neurons_4, 1]))
-bias_out = tf.Variable(bias_initializer([1]))
+# Defining X and y from the Dataset
+X = df_opt.iloc[:,:-1]
+y = df_opt.Earning
 
-# Hidden layer
-hidden_1 = tf.nn.relu(tf.add(tf.matmul(X, W_hidden_1), bias_hidden_1))
-hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, W_hidden_2), bias_hidden_2))
-hidden_3 = tf.nn.relu(tf.add(tf.matmul(hidden_2, W_hidden_3), bias_hidden_3))
-hidden_4 = tf.nn.relu(tf.add(tf.matmul(hidden_3, W_hidden_4), bias_hidden_4))
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.10,random_state=0)
 
-# Output layer (transpose!)
-out = tf.transpose(tf.add(tf.matmul(hidden_4, W_out), bias_out))
+backwardEle(X, y)
 
-# Cost function
-mse = tf.reduce_mean(tf.squared_difference(out, Y))
+# Here We can see X14 i.e. "Native-country" is having P value > 0.05
+# o better to eleminate this column
+X_train.drop(['Native-country'], axis=1, inplace=True)
 
-# Optimizer
-opt = tf.train.AdamOptimizer().minimize(mse)
+# Same time drop this column from test set as well
+X_test.drop(['Native-country'], axis=1, inplace=True)
 
-# Init
-net.run(tf.global_variables_initializer())
+X_train.head()
 
-# Setup plot
-plt.ion()
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-line1, = ax1.plot(y_test)
-line2, = ax1.plot(y_test * 0.5)
-plt.show()
+backwardEle(X, y)
 
-# Fit neural net
-batch_size = 256
-mse_train = []
-mse_test = []
+# After eleminating 'Native-country', there is no more columns which are having P value > 0.05
+# i.e. No more eleminations are required.
 
-# Run
-epochs = 10
-for e in range(epochs):
+# Applying Logistic regression
+# Logistic Regression 
 
-    # Shuffle training data
-    shuffle_indices = np.random.permutation(np.arange(len(y_train)))
-    X_train = X_train[shuffle_indices]
-    y_train = y_train[shuffle_indices]
+from sklearn.linear_model import LogisticRegression
+classifier=LogisticRegression()
+classifier.fit(X_train,y_train)
+scor=classifier.score(X_train,y_train)
+scor
 
-    # Minibatch training
-    for i in range(0, len(y_train) // batch_size):
-        start = i * batch_size
-        batch_x = X_train[start:start + batch_size]
-        batch_y = y_train[start:start + batch_size]
-        # Run optimizer with batch
-        net.run(opt, feed_dict={X: batch_x, Y: batch_y})
 
-        # Show progress
-        if np.mod(i, 50) == 0:
-            # MSE train and test
-            mse_train.append(net.run(mse, feed_dict={X: X_train, Y: y_train}))
-            mse_test.append(net.run(mse, feed_dict={X: X_test, Y: y_test}))
-            print('MSE Train: ', mse_train[-1])
-            print('MSE Test: ', mse_test[-1])
-            # Prediction
-            pred = net.run(out, feed_dict={X: X_test})
-            line2.set_ydata(pred)
-            plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
-            plt.pause(0.01)
+# improvement required
+# Cross validation from SKlearn
+
+from sklearn.cross_validation import cross_val_score
+cv = cross_val_score(estimator = classifier, X=X,y=y,scoring='accuracy',cv=50)
+print(cv.mean())
+
+# DecisionTreeClassifier
+
+from sklearn.tree import DecisionTreeClassifier
+dtree = DecisionTreeClassifier(criterion='entropy', max_depth=5, random_state=0)
+dtree.fit(X_train, y_train)
+tscore=dtree.score(X_test, y_test)
+print(tscore)
+
+
+# RandomForestRegressor
+
+# Try different numbers of n_estimators - this will take a while or so
+from sklearn.ensemble import RandomForestClassifier
+regr_rf = RandomForestClassifier(max_depth=30, random_state=2)
+
+estimators = np.arange(100, 200, 10)
+scores = []
+for n in estimators:
+    regr_rf.set_params(n_estimators=n)
+    regr_rf.fit(X_train, y_train)
+    scores.append(regr_rf.score(X_test, y_test))
+    #print(scores)
+#max_sc_idx = scores.index(max(scores))
+print(max(scores))
+
+
+
+# Bagging
+from sklearn.ensemble import BaggingClassifier
+bg=BaggingClassifier(DecisionTreeClassifier(),n_estimators=20, max_samples = 0.5, max_features=1.0)
+bg.fit(X_train,y_train)
+bgscore=bg.score(X_test,y_test)
+print(bgscore)
+
+# Boosting
+from sklearn.ensemble import AdaBoostClassifier
+bo=AdaBoostClassifier(n_estimators=50, learning_rate=1.)
+bo.fit(X_train,y_train)
+boscore=bo.score(X_test,y_test)
+print(bgscore)
+
+#fitting XGBoost to the Training set
+import xgboost
+classifier = xgboost.XGBClassifier()
+classifier.fit(X_train,y_train)
+cscore=classifier.score(X_train,y_train)
+print(cscore)
